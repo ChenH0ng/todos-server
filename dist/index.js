aa07c2e524bced8102c8eed6ae9b6c855a803cbb
ChenH0ng <chenh0ng@outlook.com>
2017-03-12 20:06:06 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var _express=require("express"),_express2=_interopRequireDefault(_express),app=(0,_express2.default)();app.get("*",function(e,t){t.send("tech everything")}),app.listen(80,function(){console.log("start")});
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var _express=require("express"),_express2=_interopRequireDefault(_express),app=(0,_express2.default)();app.get("*",function(e,r){r.send("tech everything")}),app.listen(80);
